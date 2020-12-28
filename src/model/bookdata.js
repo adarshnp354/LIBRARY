6b2feb673127c97a2eb7333bfc0f68d0c631d9e7
adarshnp354 <74529927+adarshnp354@users.noreply.github.com>
2020-12-28 17:05:00 +0530

    Update bookdata.js

--- src/model/bookdata.js
+++ src/model/bookdata.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/libraryDb',
-{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
+mongoose.connect('mongodb://localhost:27017/libraryDb')
 
 
 const Schema=mongoose.Schema;
